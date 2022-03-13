e2ed18563d535523325531ebfefb260ae0aa0ab7
TanookiTravis <tanookitravis@gmail.com>
2022-03-13 11:44:09 -0600

    style changes, sticky logo

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,17 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+// document.getElementById shorthand function
+var $ = function( id ) { return document.getElementById( id ); };
+
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    $("navbar-sticky").classList.add("viewable");
+  } else {
+    $("navbar-sticky").classList.remove("viewable");
+  }
+}
