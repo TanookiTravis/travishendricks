08db6da97bd81573f2c7577b0c4ec8d265d97e76
TanookiTravis <tanookitravis@gmail.com>
2022-03-31 14:44:58 -0600

    added a splash section and image

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,16 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // document.getElementById shorthand function
-var $ = function( id ) { return document.getElementById( id ); };
+var byId = function( id ) { return document.getElementById( id ); };
+// document.querySelector shorthand function
+var bySelector = function( selector ) { return document.querySelector( selector ); };
 
 
 
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
-    $("navbar-sticky").classList.add("viewable");
+  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+    byId("navbar-toasty").classList.add("viewable");
   } else {
-    $("navbar-sticky").classList.remove("viewable");
+    byId("navbar-toasty").classList.remove("viewable");
   }
 }
